81a0e62fb26c47bfd6636d2c6516994dab16521a
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-10 15:48:09 +0000

    refactor(debug) type enum now defined in `__types.lua`

--- __types.lua
+++ __types.lua
@@ -1,3 +1,11 @@
+---@enum PrintColour
+PrintColour = {
+    error = '^1[Error]^7',
+    warn = '^3[Warning]^7',
+    info = '^2[Info]^7',
+    debug = '^6[Debug]^7',
+}
+
 ---@enum FiringModes
 FiringModes = {
     Automatic = 0,
