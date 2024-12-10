3964c9e91f72581e11fb6701c674042eea879f27
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-10 15:57:37 +0000

    fix(utils) debug prio type being undefined

--- shared/utils.lua
+++ shared/utils.lua
@@ -5,5 +5,5 @@ local config = require "config.shared"
 function Debug(message, priority)
     if not config.debug then return end
     priority = priority or PrintColour.info
-    print(("^5[FiveForge Debug] ^7- %s %s"):format(PrintColour[priority], message))
+    print(("^5[FiveForge Debug] ^7- %s %s"):format(priority, message))
 end
